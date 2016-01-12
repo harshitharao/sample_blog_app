c5259718e3542ab19eddfd2eff07cf4e2bee5164
harshitr <harshitr@thoughtworks.com>
2016-01-12 19:46:55 +0530

    Creating tag model,view and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112140214) do
+ActiveRecord::Schema.define(version: 20160112141414) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
