b179ca5fd6cea0bc05f1a475ae16b65ba7320b73
harshitr <harshitr@thoughtworks.com>
2016-01-12 20:18:28 +0530

    Creating title words frequencies method and tests

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,12 @@
 class Blog < ActiveRecord::Base
   has_and_belongs_to_many :tags
+
+  def fetch_title_word_frequencies
+    title_words = Blog.all.pluck(:title).join(' ').split(' ')
+    words_hash = Hash.new(0)
+    title_words.each do |word|
+      words_hash[word.downcase] += 1
+    end
+    words_hash
+  end
 end
--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should fetch title word frequencies wither for lowercase or uppercase words" do
+    Blog.create title: 'rails basics', content: 'rails content'
+    Blog.create title: 'advanced Rails', content: 'rails content full'
+
+    hashObject = Blog.new.fetch_title_word_frequencies
+
+    assert_equal 2, hashObject['rails']
+    assert_equal 1, hashObject['basics']
+  end
 end
